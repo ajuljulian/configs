7f6200ac3faf8ec9553e2fa54b67a9949cd3d8ff
Ara Juljulian <ajuljulian@yahoo.com>
2022-05-19 11:43:59 -0700

    Improve nvim-cmp navigation

--- nvim/lua/plugins/nvim-cmp-config.lua
+++ nvim/lua/plugins/nvim-cmp-config.lua
@@ -1,11 +1,12 @@
 local cmp = require "cmp"
 local lspkind = require("lspkind")
 
-local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+local has_words_before = function()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+local luasnip = require("luasnip")
 cmp.setup(
     {
         snippet = {
@@ -26,15 +27,12 @@ cmp.setup(
             },
             ["<Tab>"] = cmp.mapping(
                 function(fallback)
-                    local luasnip = require "luasnip"
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif luasnip.expandable() then
-                        luasnip.expand()
                     elseif luasnip.expand_or_jumpable() then
                         luasnip.expand_or_jump()
-                    elseif check_backspace() then
-                        fallback()
+                    elseif has_words_before() then
+                        cmp.complete()
                     else
                         fallback()
                     end
@@ -43,15 +41,10 @@ cmp.setup(
             ),
             ["<S-Tab>"] = cmp.mapping(
                 function(fallback)
-                    local luasnip = require "luasnip"
                     if cmp.visible() then
                         cmp.select_prev_item()
-                    elseif luasnip.expandable() then
-                        luasnip.expand()
-                    elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif check_backspace() then
-                        fallback()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
                     else
                         fallback()
                     end
