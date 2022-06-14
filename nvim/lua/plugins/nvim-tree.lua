24069179743928fd53a488c3ba5369da5d64dce3
Ara Juljulian <ajuljulian@yahoo.com>
2022-06-13 19:26:36 -0700

    Remove nvim-tree autocommand

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,4 @@
 require "nvim-tree".setup {}
 
 -- Automatically close the tab/vim when nvim-tree is the last window in the tab
-vim.cmd [[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
+-- vim.cmd [[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]]
